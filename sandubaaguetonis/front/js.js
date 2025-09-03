5533362ad59f64aa0d52ae6849d61d5bc92f9930
Welisson Ferreira <welissonferreira2000@gmail.com>
2025-09-02 22:45:33 -0300

    Update js.js

--- sandubaaguetonis/front/js.js
+++ sandubaaguetonis/front/js.js
@@ -2352,7 +2352,7 @@ btnFinalizarPedidoWhatsApp.addEventListener('click', () => {
     let btnZap = document.getElementById('botaozap')
     btnZap.addEventListener('click', function() {
 
-    const numeroWhatsApp = '5582987165196'; // Exemplo: 55 = Brasil, 82 = DDD, 999261614 = número
+    const numeroWhatsApp = '5517981321172'; // Exemplo: 55 = Brasil, 82 = DDD, 999261614 = número
 
     // 2. Crie a mensagem (opcional, mas muito útil)
     const mensagemPadrao = 'Olá, gostaria de fazer um pedido!';
